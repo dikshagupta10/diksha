7287ab3fb53866d6b8c71faf215ae2b725e809fc
dikshagupta10 <80531363+dikshagupta10@users.noreply.github.com>
2021-03-13 09:31:33 +0530

    Create app.module.ts

---
+++ http/src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from "@angular/common/http";
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { GithubService } from './github.service';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,HttpClientModule
+  ],
+  providers: [GithubService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
